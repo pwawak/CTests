e9a2b1b2f56ee0640b4f1e3bcc072cd13a455aed
piotr <pwawak@gmail.com>
2018-11-17 21:39:15 +0100

    Task 5_11

--- test_5_11.c
+++ test_5_11.c
@@ -1,1 +1,23 @@
 #include  <stdio.h>
+
+int	main( void )
+{
+	char 	arr[10];
+	int		num = 0;
+
+	for( int i=0; i<10; i++ )
+		arr[i] = 0;
+
+	printf( "Podaj liczby: " );
+	do {
+		scanf( "%d", &num );
+
+		if( num >= 0 and num <= 10 )
+			++arr[ num ];
+	} while ( num != -1 );
+
+	for( int i=0; i<10; i++ )
+		printf( "%d - %d\n", i, arr[i] );
+
+	return 0;
+}
